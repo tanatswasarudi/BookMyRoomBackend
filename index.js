e36d7bc546092d04729be6ef49d55c7a7c92fddb
tanatswasarudi <tanatswasarudi09@gmail.com>
2023-07-03 12:35:16 +0530

    filter

--- index.js
+++ index.js
@@ -102,6 +102,7 @@ app.post("/login", async (req, res) => {
     address : String,
     photos : [String],
     perks : [String],
+    category : String,
     description : String,
     price: String,
     MaxGuests: String,
